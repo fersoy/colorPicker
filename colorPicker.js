6cbea49c818bf31c4c12e7df795a970a38262a19
Fatih Ersoy <fersoy@HSSHIGH.ORG>
2019-08-23 15:13:01 -0500

    initial commit

--- colorPicker.js
+++ colorPicker.js
@@ -7,9 +7,25 @@ var colors = [
     "rgb(255, 0, 255)"
    ]
    var colorBoxes = document.querySelectorAll('.colorBox');
-   var pickedColoir = colors[3];
+   var pickedColor = colors[3];
    var colorDisplay = document.getElementById('colorDisplay');
+   colorDisplay.textContent = pickedColor;
+   var messageDisplay = document.getElementById('messageDisplay');
+
+ 
 
    for (var i = 0; i < colorBoxes.length; i++){
+
        colorBoxes[i].style.backgroundColor = colors[i];
+
+       colorBoxes[i].addEventListener('click', function(){
+           var clickedColor = this.style.backgroundColor;
+           if(clickedColor === pickedColor){
+            messageDisplay.textContent = 'Correct!'
+               }
+               else{
+                this.style.backgroundColor = '#98b6ad';
+                messageDisplay.textContent = 'Try Again!'
+           }
+       })
    }
