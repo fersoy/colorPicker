c9ad184781d29ae0713d67be7a7d40046b459608
Fatih Ersoy <fersoy@HSSHIGH.ORG>
2019-08-27 13:35:33 -0500

    logic is working

--- colorPicker.js
+++ colorPicker.js
@@ -1,16 +1,10 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-   ]
-   var colorBoxes = document.querySelectorAll('.colorBox');
-   var pickedColor = colors[3];
-   var colorDisplay = document.getElementById('colorDisplay');
-   colorDisplay.textContent = pickedColor;
-   var messageDisplay = document.getElementById('messageDisplay');
+var colors = generateRandomColors(6);
+var h1 = document.querySelector('h1');
+var colorBoxes = document.querySelectorAll('.colorBox');
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById('colorDisplay');
+colorDisplay.textContent = pickedColor;
+var messageDisplay = document.getElementById('messageDisplay');
 
  
 
@@ -22,10 +16,35 @@ var colors = [
            var clickedColor = this.style.backgroundColor;
            if(clickedColor === pickedColor){
             messageDisplay.textContent = 'Correct!'
+            changeColors(clickedColor);
+            h1.style.background = clickedColor;
                }
                else{
                 this.style.backgroundColor = '#98b6ad';
                 messageDisplay.textContent = 'Try Again!'
            }
        })
+   }
+   function changeColors(color){
+       for (var i = 0; i < colorBoxes.length; i++){
+         colorBoxes[i].style.background = color;  
+       }
+   }
+   function pickColor(){
+     var random = Math.floor(Math.random() * colors.length);
+     return colors[random];
+   }
+   function generateRandomColors(num){
+       var arr = [];
+       for (var i = 0; i < num; i++){
+           arr.push(randomColor())
+       } 
+       return arr;
+   }
+
+   function randomColor(){
+       var r = Math.floor(Math.random() * 256);
+       var g = Math.floor(Math.random() * 256);
+       var b = Math.floor(Math.random() * 256);
+return "rgb(" + r + ", " + g + ", " + b + ")";
    }
